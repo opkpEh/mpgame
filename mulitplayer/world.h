db42808749df6547768aba87d157eaf3d313178d
Kushagra Pandey <56930383+opkpEh@users.noreply.github.com>
2024-01-07 17:22:54 +0530

    Update world.h

--- mulitplayer/world.h
+++ mulitplayer/world.h
@@ -1,98 +1,102 @@
 #pragma once
 #include "raylib.h"
-#include "main.h"
+#include <vector>
+
 namespace world
 {
-    struct grass
+    struct mapGen
     {
-        Texture2D tileTexture;
-        Rectangle tileSource;
-        Rectangle tileDest;
+        Texture2D grassTexture;
+        Rectangle grassSource;
+        Rectangle grassDest;
 
-        enum class tileType
+        enum class TerrainType
         {
-            grass1 = 1,
-            grass2= 2,
-            grass3 = 3,
-            grass4 = 4,
-            grass5 = 5,
-            grass6= 6,
-            grass7 = 7,
-            flower1 = 8,
-            flower2 = 9,
+            Grass,
+            Rock,
+            Bush,
+            Tree
         };
 
-        const static int rows = (int)worldWidth/16;  
-        const static int cols = (int)worldHeight/16;
+        int rows;
+        int cols;
+        
+        std::vector<std::vector<TerrainType>> map;
 
-        int mapMatrix[rows][cols];
+        void generateTerrain(TerrainType terrain, int count) {
+            while (count > 0) {
+                int row = rand() % rows;
+                int col = rand() % cols;
 
-        void generateMap()
-        {
-            for (int i = 0; i < rows; ++i)
-            {
-                for (int j = 0; j < cols; ++j)
-                {
-                    mapMatrix[i][j] = GetRandomValue(1, 9);
+                if (map[row][col] == TerrainType::Grass) {
+                    map[row][col] = terrain;
+                    count--;
                 }
             }
         }
 
-        void renderMap()
-        {
-            for (int i = 0; i < rows; ++i)
-            {
-                for (int j = 0; j < cols; ++j)
-                {
-                    switch (static_cast<tileType>(mapMatrix[i][j]))
-                    {
-                    case tileType::grass1:
-                        tileSource.x = 48;
-                        tileSource.y = 0;
+        void generateMap() {
+            srand(static_cast<unsigned>(time(nullptr)));
+
+            generateTerrain(TerrainType::Grass, rows * cols / 2);
+            generateTerrain(TerrainType::Rock, rows * cols / 8);
+            generateTerrain(TerrainType::Bush, rows * cols / 8);
+            generateTerrain(TerrainType::Tree, rows * cols / 8);
+        }
+
+
+        void initMap(int _rows, int _cols) {
+            rows = _rows;
+            cols = _cols;
+            map.resize(rows, std::vector<TerrainType>(cols, TerrainType::Grass));
+            generateMap();
+        }
+
+        void displayMap() const {
+            for (const auto& row : map) {
+                for (const auto& cell : row) {
+                    char symbol = 0;
+                    switch (cell) {
+                    case TerrainType::Grass: symbol = '.'; break;
+                    case TerrainType::Rock: symbol = '#'; break;
+                    case TerrainType::Bush: symbol = '*'; break;
+                    case TerrainType::Tree: symbol = 'T'; break;
+                    }
+                    std::cout << symbol << ' ';
+                }
+                std::cout << std::endl;
+            }
+        }
+
+        void renderMap() {
+            for (int i = 0; i < rows; ++i) {
+                for (int j = 0; j < cols; ++j) {
+                    switch (map[i][j]) {
+                    case TerrainType::Grass:
+                        grassSource.x = 0;
+                        grassSource.y = 0;
                         break;
-                    case tileType::grass2:
-						tileSource.x = 64;
-						tileSource.y = 0;
-						break;
-                    case tileType::grass3:
-                        tileSource.x = 80;
-                        tileSource.y = 0;
+                    case TerrainType::Rock:
+                        grassSource.x = 80;
+                        grassSource.y = 0;
                         break;
-                    case tileType::grass4:
-                        tileSource.x = 48;
-                        tileSource.y = 16;
+                    case TerrainType::Bush:
+                        grassSource.x = 64;
+                        grassSource.y = 16;
                         break;
-                    case tileType::grass5:
-                        tileSource.x = 64;
-                        tileSource.y = 16;
-                        break;
-                    case tileType::grass6:
-						tileSource.x = 80;
-						tileSource.y = 16;
-						break;
-                    case tileType::grass7:
-                        tileSource.x = 48;
-						tileSource.y = 32;
-						break;
-                    case tileType::flower1:
-                        tileSource.x = 64;
-                        tileSource.y = 32;
-                        break;
-                    case tileType::flower2:
-                        tileSource.x = 80;
-                        tileSource.y = 32;
+                    case TerrainType::Tree:
+                        grassSource.x = 80;
+                        grassSource.y = 16;
                         break;
                     }
 
-                    tileDest.x = (float)(j * 32);  
-                    tileDest.y = (float)(i * 32);  
+                    grassDest.x = static_cast<float>(j * grassDest.height);
+                    grassDest.y = static_cast<float>(i * grassDest.width);
 
-                    DrawTexturePro(tileTexture, tileSource, tileDest, { 0, 0 }, 0, WHITE);
+                    DrawTexturePro(grassTexture, grassSource, grassDest, { 0, 0 }, 0, WHITE);
                 }
+
             }
         }
-
-        
-
     };
 }
